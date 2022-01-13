6a084dcb8ab40395effb5d2e3e339d0ba41fba7c
ljg95 <ljg95924@github.com>
2022-01-13 11:48:59 +0900

    디폴트메소드, 정적메소드 사용

---
+++ src/ThisIsJavaChapter08/RemoteControlExample.java
@@ -0,0 +1,18 @@
+package ThisIsJavaChapter08;
+
+public class RemoteControlExample {
+    public static void main(String[] args) {
+        RemoteControl rc = null;
+        rc = new Television();
+        rc.turnOn();
+        rc.setMute(true);
+        rc.turnOff();
+
+        rc = new Audio();
+        rc.turnOn();
+        rc.setMute(true);
+        rc.turnOff();
+
+        RemoteControl.changeBattery();
+    }
+}
