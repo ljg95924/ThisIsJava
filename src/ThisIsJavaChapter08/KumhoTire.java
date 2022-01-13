d3cdbbe2e834b55311d0bc873379e12c4ba11023
ljg95 <ljg95924@github.com>
2022-01-13 11:51:43 +0900

    필드 다형성

---
+++ src/ThisIsJavaChapter08/KumhoTire.java
@@ -0,0 +1,9 @@
+package ThisIsJavaChapter08;
+
+public class KumhoTire implements Tire {
+
+    @Override
+    public void roll() {
+        System.out.println("금호타이가 굴러갑니다~");
+    }
+}
---
+++ src/ThisIsJavaChapter08/Tire.java
@@ -0,0 +1,5 @@
+package ThisIsJavaChapter08;
+
+public interface Tire {
+    public void roll();
+}
