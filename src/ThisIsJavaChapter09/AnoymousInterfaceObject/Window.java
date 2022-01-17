e775074d9943cfdbd96e39162f39c71bc54e17a5
ljg95 <ljg95924@github.com>
2022-01-17 14:57:15 +0900

    UI프로그램 버튼클릭 이벤트처리 익명구현객체

---
+++ src/ThisIsJavaChapter09/AnoymousInterfaceObject/Window.java
@@ -0,0 +1,27 @@
+package ThisIsJavaChapter09.AnoymousInterfaceObject;
+
+public class Window {
+    Button button1 = new Button();
+    Button button2 = new Button();
+
+    //필드초기값으로 대입
+    Button.OnClickListener listener = new Button.OnClickListener() {
+        @Override
+        public void onClick() {
+            System.out.println("전화를 겁니다.");
+        }
+    };
+
+    
+    Window() {
+        //매개값으로 필드대입
+        button1.setOnClickListener(listener);
+        //매개값으로 익명구현객체대입
+        button2.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick() {
+                System.out.println("메시지를 보냅니다.");
+            }
+        });
+    }
+}
