3b3cf0489385e4365a652d15ac404188f115d008
ljg95 <ljg95924@github.com>
2022-01-18 13:38:57 +0900

    다중catch문

--- src/ThisIsJavaChapter10/MultipleCatchExaceptionExample.java
+++ src/ThisIsJavaChapter10/MultipleCatchExaceptionExample.java
@@ -1,2 +1,19 @@
-package ThisIsJavaChapter10;public class MultipleCatchExaceptionExample {
+package ThisIsJavaChapter10;
+
+public class MultipleCatchExaceptionExample {
+    public static void main(String[] args) {
+        try{
+            String data1 = args[0];
+            String data2 = args[1];
+            int value1 = Integer.parseInt(data1);
+            int value2 = Integer.parseInt(data2);
+            System.out.println(data1 + data2);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("실행 매개값의 수가 부족");
+        }catch (NumberFormatException e){
+            System.out.println("숫자로 변환 불가능");
+        }finally {
+            System.out.println("다시 실행하셈");
+        }
+    }
 }
