2b5d1c9c3d9d375f27a21c88a1853eba41c0347f
Lee JaeGeun <ljg95924@github.com>
2022-01-18 18:34:30 +0900

    hashcode() 객체해시코드

--- src/ThisIsJavaChapter11/hashCode/Key.java
+++ src/ThisIsJavaChapter11/hashCode/Key.java
@@ -1,2 +1,27 @@
-package ThisIsJavaChapter11.hashCode;public class Key {
+package ThisIsJavaChapter11.hashCode;
+
+import java.util.Objects;
+
+public class Key {
+    public int number;
+
+    public Key(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Key) {
+            Key comparyKey = (Key) o;
+            if (this.number == comparyKey.number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
 }
--- src/ThisIsJavaChapter11/hashCode/KeyExample.java
+++ src/ThisIsJavaChapter11/hashCode/KeyExample.java
@@ -1,2 +1,14 @@
-package ThisIsJavaChapter11.hashCode;public class KeyExample {
+package ThisIsJavaChapter11.hashCode;
+
+import java.util.HashMap;
+
+public class KeyExample {
+    public static void main(String[] args) {
+        HashMap<Key, String> hashMap = new HashMap<Key, String>();
+
+        hashMap.put(new Key(1),"홍길동");
+
+        System.out.println(hashMap.get(new Key(1)));
+
+    }
 }
