824107407281cfb0439322946f6eff055f8740f3
Lee JaeGeun <ljg95924@github.com>
2022-01-18 19:30:14 +0900

    객체소멸자(finalize())

---
+++ src/ThisIsJavaChapter11/Finalize/FinalizeExample.java
@@ -0,0 +1,17 @@
+package ThisIsJavaChapter11.Finalize;
+
+public class FinalizeExample {
+    public static void main(String[] args) {
+        Counter counter = null;
+
+        for(int i=1; i<=500; i++){
+            counter = new Counter(i);
+            
+            //counter 객체를 쓰레기로 만듬
+            counter = null;
+            //쓰레기 수집기 실행 요청
+            System.gc();
+        }
+
+    }
+}
