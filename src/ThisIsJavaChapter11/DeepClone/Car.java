37aa8e786b08c7d32e38fe679a3afbb060565a02
Lee JaeGeun <ljg95924@github.com>
2022-01-18 18:36:07 +0900

    clone() 객체 깊은복제

--- src/ThisIsJavaChapter11/DeepClone/Car.java
+++ src/ThisIsJavaChapter11/DeepClone/Car.java
@@ -1,2 +1,9 @@
-package ThisIsJavaChapter11.DeepClone;public class Car {
+package ThisIsJavaChapter11.DeepClone;
+
+public class Car {
+    public String model;
+
+    public Car(String model) {
+        this.model = model;
+    }
 }
