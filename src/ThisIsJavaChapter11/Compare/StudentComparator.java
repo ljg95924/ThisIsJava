6d78a621efbf517d6bef786c7dfda1381bcbe3b2
Lee JaeGeun <ljg95924@github.com>
2022-01-18 20:10:36 +0900

    객체비교 comapare()

---
+++ src/ThisIsJavaChapter11/Compare/StudentComparator.java
@@ -0,0 +1,12 @@
+package ThisIsJavaChapter11.Compare;
+
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator<CompareExample.Student> {
+
+
+    @Override
+    public int compare(CompareExample.Student o1, CompareExample.Student o2) {
+        return Integer.compare(o1.sno, o2.sno);
+    }
+}
