2e39d926a7270f278aa2d71254a64639883ab0d4
Lee JaeGeun <ljg95924@github.com>
2022-01-19 12:55:16 +0900

    가비지컬렉터 gc()

--- src/ThisIsJavaChapter11/GarbageCollector/GcExample.java
+++ src/ThisIsJavaChapter11/GarbageCollector/GcExample.java
@@ -1,2 +1,31 @@
-package ThisIsJavaChapter11.GcExample;public class GcExample {
+package ThisIsJavaChapter11.GarbageCollector;
+
+public class GcExample {
+    public static void main(String[] args) {
+        Employee emp;
+
+        emp = new Employee(1); //쓰레기
+        emp = null;
+        emp = new Employee(2); //쓰레기
+        emp = new Employee(3);
+
+        System.out.println("최종적으로 참조하는 사원번호: " + emp.eno);
+        System.gc();
+    }
+}
+
+class Employee {
+    int eno;
+
+    public Employee(int eno) {
+        this.eno = eno;
+        System.out.println("Employee(" + eno + ") 이 메모리에 생성됨");
+    }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Employee(" + eno + ") 이 메모리에서 제거됨");
+    }
+
 }
