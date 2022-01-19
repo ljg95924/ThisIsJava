da8cdf69d00b9413e77af5abdb86fd6280e21684
Lee JaeGeun <ljg95924@github.com>
2022-01-19 12:57:16 +0900

    Class 객체 얻기 getName(), forName()

--- src/ThisIsJavaChapter11/GetClassAndForName/ClassExample.java
+++ src/ThisIsJavaChapter11/GetClassAndForName/ClassExample.java
@@ -1,2 +1,24 @@
-package ThisIsJavaChapter11.GetClassAndForName;public class ClassExample {
+package ThisIsJavaChapter11.GetClassAndForName;
+
+import ThisIsJavaChapter10.Account;
+
+public class ClassExample {
+    public static void main(String[] args) {
+        Account account = new Account();
+        Class clazz1 = account.getClass();
+        System.out.println(clazz1.getName());
+        System.out.println(clazz1.getSimpleName());
+        System.out.println(clazz1.getPackage().getName());
+        System.out.println();
+
+        try {
+            Class clazz2 = Class.forName("ThisIsJavaChapter10.Account");
+            System.out.println(clazz2.getName());
+            System.out.println(clazz2.getSimpleName());
+            System.out.println(clazz2.getPackage().getName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
