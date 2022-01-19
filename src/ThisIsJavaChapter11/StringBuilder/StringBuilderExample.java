60d8d83f01ea144bbdde2ead4f0d5be8136524b9
Lee JaeGeun <ljg95924@github.com>
2022-01-19 17:44:34 +0900

    StringBuilder

--- src/ThisIsJavaChapter11/StringBuilder/StringBuilderExample.java
+++ src/ThisIsJavaChapter11/StringBuilder/StringBuilderExample.java
@@ -1,2 +1,26 @@
-package ThisIsJavaChapter11.StringBuilder;public class StringBuilderExample {
+package ThisIsJavaChapter11.StringBuilder;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Java");
+        stringBuilder.append("Program");
+        System.out.println(stringBuilder.toString());
+
+        stringBuilder.insert(4, "2");
+        System.out.println(stringBuilder.toString());
+
+        stringBuilder.setCharAt(4, '6');
+        System.out.println(stringBuilder.toString());
+
+        stringBuilder.replace(6, 13, "Book");
+        System.out.println(stringBuilder.toString());
+
+        stringBuilder.delete(4, 5);
+        System.out.println(stringBuilder.toString());
+
+        String result = stringBuilder.toString();
+        System.out.println(result);
+    }
 }
