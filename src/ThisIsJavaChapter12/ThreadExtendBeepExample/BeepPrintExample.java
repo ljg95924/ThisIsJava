27c550306f66215c327239fd5798f0cd2d5a3476
Lee JaeGeun <ljg95924@github.com>
2022-01-20 18:21:37 +0900

    Thread 하위 클래스로부터 생성 예제

--- src/ThisIsJavaChapter12/ThreadExtendBeepExample/BeepPrintExample.java
+++ src/ThisIsJavaChapter12/ThreadExtendBeepExample/BeepPrintExample.java
@@ -1,2 +1,17 @@
-package ThisIsJavaChapter12.ThreadExtendBeepExample;public class BeepPrintExample {
+package ThisIsJavaChapter12.ThreadExtendBeepExample;
+
+public class BeepPrintExample {
+    public static void main(String[] args) {
+        Thread thread = new BeepThread();
+        thread.start();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("띵");
+            try {
+                Thread.sleep(500);
+            } catch (Exception e) {
+
+            }
+        }
+    }
 }
--- src/ThisIsJavaChapter12/ThreadExtendBeepExample/BeepThread.java
+++ src/ThisIsJavaChapter12/ThreadExtendBeepExample/BeepThread.java
@@ -1,2 +1,18 @@
-package ThisIsJavaChapter12.ThreadExtendBeepExample;public class BeepThread {
+package ThisIsJavaChapter12.ThreadExtendBeepExample;
+
+import java.awt.*;
+
+public class BeepThread extends Thread{
+    @Override
+    public void run() {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        for(int i=0; i<5; i++){
+            toolkit.beep();
+            try{
+                Thread.sleep(500);
+            }catch (Exception e){
+
+            }
+        }
+    }
 }
