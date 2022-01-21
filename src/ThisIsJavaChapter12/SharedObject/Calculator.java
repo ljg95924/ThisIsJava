7cacf3e90a9866a58c7f8890ed2faaa9a0e39873
Lee JaeGeun <ljg95924@github.com>
2022-01-21 14:37:43 +0900

    공유객체를 사용할때 주의할점

--- src/ThisIsJavaChapter12/SharedObject/Calculator.java
+++ src/ThisIsJavaChapter12/SharedObject/Calculator.java
@@ -1,2 +1,19 @@
-package ThisIsJavaChapter12.SharedObject;public class Calculator {
+package ThisIsJavaChapter12.SharedObject;
+
+public class Calculator {
+    private int memory;
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+    }
 }
--- src/ThisIsJavaChapter12/SharedObject/User1.java
+++ src/ThisIsJavaChapter12/SharedObject/User1.java
@@ -1,2 +1,15 @@
-package ThisIsJavaChapter12.SharedObject;public class User1 {
+package ThisIsJavaChapter12.SharedObject;
+
+public class User1 extends Thread {
+    private Calculator calculator;
+
+    public void setCalculator(Calculator calculator) {
+        this.setName("User1");
+        this.calculator = calculator;
+    }
+
+    @Override
+    public void run() {
+        calculator.setMemory(500);
+    }
 }
