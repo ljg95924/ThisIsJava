8b60a103902daceebca30fe8050bd58fd713b297
Lee JaeGeun <ljg95924@github.com>
2022-01-21 16:46:35 +0900

    데몬스레드예제

---
+++ src/ThisIsJavaChapter12/DaemonThread/DaemonExample.java
@@ -0,0 +1,16 @@
+package ThisIsJavaChapter12.DaemonThread;
+
+public class DaemonExample {
+    public static void main(String[] args) {
+        AutoSaveThread autoSaveThread = new AutoSaveThread();
+        autoSaveThread.setDaemon(true);
+        autoSaveThread.start();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("메인스레드 종료");
+    }
+}
