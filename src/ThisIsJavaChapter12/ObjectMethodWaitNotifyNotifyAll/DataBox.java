0fe89074671af84d62cadfdbc6eae33f511cd551
Lee JaeGeun <ljg95924@github.com>
2022-01-21 15:56:39 +0900

    WaitNotify 예제2

---
+++ src/ThisIsJavaChapter12/ObjectMethodWaitNotifyNotifyAll/DataBox.java
@@ -0,0 +1,35 @@
+package ThisIsJavaChapter12.ObjectMethodWaitNotifyNotifyAll;
+
+public class DataBox {
+    private String data;
+
+    public synchronized String getData() {
+        if (this.data == null) {
+            try {
+                System.out.println("getData정지정지");
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        String returnValue = data;
+        System.out.println("읽은 데이터: " + returnValue);
+        data = null;
+        notify();
+        return returnValue;
+    }
+
+    public synchronized void setData(String data) {
+        if (this.data != null) {
+            try {
+                System.out.println("setData정지정지");
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        this.data = data;
+        System.out.println("생성한 데이터: " + data);
+        notify();
+    }
+}
