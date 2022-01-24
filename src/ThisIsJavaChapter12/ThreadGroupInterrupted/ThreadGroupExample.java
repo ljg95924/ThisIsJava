5dc875722f41c931c586e7e6a5af5963d8808d2d
Lee JaeGeun <ljg95924@github.com>
2022-01-24 16:00:22 +0900

    스래드 그룹 interrupted()

---
+++ src/ThisIsJavaChapter12/ThreadGroupInterrupted/ThreadGroupExample.java
@@ -0,0 +1,25 @@
+package ThisIsJavaChapter12.ThreadGroupInterrupted;
+
+public class ThreadGroupExample {
+    public static void main(String[] args) {
+        ThreadGroup threadGroup = new ThreadGroup("myGroup");
+        WorkThread workThreadA = new WorkThread(threadGroup, "workThreadA");
+        WorkThread workThreadB = new WorkThread(threadGroup, "workThreadB");
+
+        workThreadA.start();
+        workThreadB.start();
+
+        System.out.println("main 스레드 그룹의 list() 메소드 출력 내용");
+        ThreadGroup mainGroup = Thread.currentThread().getThreadGroup();
+        mainGroup.list();
+        System.out.println();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+
+        }
+        System.out.println("myGroup 스레드 스룹의 interrupt() 메소드 호출");
+        threadGroup.interrupt();
+    }
+}
---
+++ src/ThisIsJavaChapter12/ThreadGroupInterrupted/WorkThread.java
@@ -0,0 +1,20 @@
+package ThisIsJavaChapter12.ThreadGroupInterrupted;
+
+public class WorkThread extends Thread {
+    public WorkThread(ThreadGroup threadGroup, String ThreadName) {
+        super(threadGroup, ThreadName);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(getName() + " interrupted");
+                break;
+            }
+        }
+        System.out.println(getName() + "종료됨");
+    }
+}
