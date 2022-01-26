811c12befbbd39e1f6e66de584ebfde01042405c
Lee JaeGeun <ljg95924@github.com>
2022-01-26 20:26:15 +0900

    멀티타입파라미터

--- src/ThisIsJavaChapter13/GenericMultiTypeParameter/Product.java
+++ src/ThisIsJavaChapter13/GenericMultiTypeParameter/Product.java
@@ -1,2 +1,22 @@
-package ThisIsJavaChapter13.GenericMultiTypeParameter;public class Product {
+package ThisIsJavaChapter13.GenericMultiTypeParameter;
+
+public class Product<T, M> {
+    private T kind;
+    private M model;
+
+    public T getKind() {
+        return this.kind;
+    }
+
+    public M getModel() {
+        return this.model;
+    }
+
+    public void setKind(T kind) {
+        this.kind = kind;
+    }
+
+    public void setModel(M model) {
+        this.model = model;
+    }
 }
--- src/ThisIsJavaChapter13/GenericMultiTypeParameter/ProductExample.java
+++ src/ThisIsJavaChapter13/GenericMultiTypeParameter/ProductExample.java
@@ -1,2 +1,23 @@
-package ThisIsJavaChapter13.GenericMultiTypeParameter;public class ProductExample {
+package ThisIsJavaChapter13.GenericMultiTypeParameter;
+
+public class ProductExample {
+    public static void main(String[] args) {
+        Product<Tv, String> product1 = new Product<Tv, String>();
+        product1.setKind(new Tv());
+        product1.setModel("스마트 Tv");
+        System.out.println(product1.getKind());
+        System.out.println(product1.getModel());
+
+        Product<Car, String> product2 = new Product<Car, String>();
+        product2.setKind(new Car());
+        product2.setModel("디젤");
+        System.out.println(product2.getKind());
+        System.out.println(product2.getModel());
+    }
+
+    private static class Tv {
+    }
+
+    private static class Car {
+    }
 }
