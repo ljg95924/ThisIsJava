a80c5f4eea1d9b1e8aedb5d69dee22fdfb3c3511
Lee JaeGeun <ljg95924@github.com>
2022-01-26 14:40:39 +0900

    콜백방식의 작업통보받기

---
+++ src/ThisIsJavaChapter12/CallbackCompletionHandler/CallbackExample.java
@@ -0,0 +1,55 @@
+package ThisIsJavaChapter12.CallbackCompletionHandler;
+
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CallbackExample {
+    private ExecutorService executorService;
+
+    public CallbackExample() {
+        executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    }
+
+    //callback메소드를 가진 CompletionHandler 객체생성
+    private CompletionHandler<Integer, Void> callback = new CompletionHandler<Integer, Void>() {
+        @Override
+        public void completed(Integer result, Void attachment) {
+            System.out.println("completed() 실행: " + result);
+        }
+
+        @Override
+        public void failed(Throwable exc, Void attachment) {
+            System.out.println("failed() 실행: " + exc.toString());
+        }
+    };
+
+    public void doWork(final String x, final String y) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int intX = Integer.parseInt(x);
+                    int intY = Integer.parseInt(y);
+                    int result = intX + intY;
+                    callback.completed(result, null);
+                } catch (NumberFormatException e) {
+                    callback.failed(e, null);
+                }
+            }
+        };
+        executorService.submit(task); //스레드풀에게 작업처리 요청
+    }
+
+    public void finish() {
+        executorService.shutdown();
+    }
+
+    public static void main(String[] args) {
+        CallbackExample example = new CallbackExample();
+        example.doWork("3", "3");
+        example.doWork("3", "삼");
+        example.finish();
+    }
+}
