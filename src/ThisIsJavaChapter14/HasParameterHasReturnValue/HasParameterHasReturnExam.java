58d5ce5b5d872bf9930960e4afc3310e086bb351
Lee JaeGeun <ljg95924@github.com>
2022-01-28 18:42:07 +0900

    리턴값이 있는 람다식 함수적 인터페이스

--- src/ThisIsJavaChapter14/HasParameterHasReturnValue/HasParameterHasReturnExam.java
+++ src/ThisIsJavaChapter14/HasParameterHasReturnValue/HasParameterHasReturnExam.java
@@ -1,2 +1,23 @@
-package ThisIsJavaChapter14.HasParameterHasReturnValue;public class MyFunctionalInterfaceExample {
+package ThisIsJavaChapter14.HasParameterHasReturnValue;
+
+public class HasParameterHasReturnExam {
+    public static void main(String[] args) {
+        MyFunctionalInterface fi;
+
+        fi = (x, y) -> {
+            int result = x + y;
+            return result;
+        };
+        System.out.println(fi.method(2, 5));
+
+        fi = (x, y) -> {
+            return x + y;
+        };
+        System.out.println(fi.method(2, 5));
+
+        fi = (x, y) -> x + y;
+        System.out.println(fi.method(2, 5));
+
+
+    }
 }
--- src/ThisIsJavaChapter14/HasParameterHasReturnValue/MyFunctionalInterface.java
+++ src/ThisIsJavaChapter14/HasParameterHasReturnValue/MyFunctionalInterface.java
@@ -1,2 +1,5 @@
-package ThisIsJavaChapter14.HasParameterHasReturnValue;public class MyFunctionalInterface {
+package ThisIsJavaChapter14.HasParameterHasReturnValue;
+
+public interface MyFunctionalInterface {
+    public int method(int x, int y);
 }
