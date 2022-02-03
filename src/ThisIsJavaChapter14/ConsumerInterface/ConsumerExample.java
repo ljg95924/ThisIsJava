da1d9ddd7df472172a9f56eb14893816b93d1acd
Lee JaeGeun <ljg95924@github.com>
2022-02-03 11:58:12 +0900

    람다식: Consumer 함수적 인터페이스

---
+++ src/ThisIsJavaChapter14/ConsumerInterface/ConsumerExample.java
@@ -0,0 +1,22 @@
+package ThisIsJavaChapter14.ConsumerInterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.ObjIntConsumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        Consumer<String> consumer = t -> System.out.println(t + "Study");
+        consumer.accept("java");
+
+        BiConsumer<String, String> biConsumer = (t, u) -> System.out.println(t + u);
+        biConsumer.accept("Java", "Study");
+
+        DoubleConsumer doubleConsumer = d -> System.out.println(3 + d);
+        doubleConsumer.accept(0.1592);
+
+        ObjIntConsumer<Integer> objIntConsumer = (t, i) -> System.out.println(t + i);
+        objIntConsumer.accept(10, 10);
+    }
+}
