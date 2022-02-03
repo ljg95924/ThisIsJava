aa76a6430e8092d5f3ad6059480b37664ce00cd2
Lee JaeGeun <ljg95924@github.com>
2022-02-03 16:04:23 +0900

    람다식: Predicate의 디폴트메소드

---
+++ src/ThisIsJavaChapter14/PredicateAndOrNegate/PredicateAndOrNegateExample.java
@@ -0,0 +1,23 @@
+package ThisIsJavaChapter14.PredicateAndOrNegate;
+
+import java.util.function.IntPredicate;
+
+public class PredicateAndOrNegateExample {
+    public static void main(String[] args) {
+        IntPredicate predicateA = a -> a % 2 == 0;
+        IntPredicate predicateB = a -> a % 3 == 0;
+
+        IntPredicate predicateAB;
+        boolean result;
+
+        predicateAB = predicateA.and(predicateB);
+        result = predicateAB.test(9);
+        System.out.println("9는 2와 3의 배수인가? " + result);
+
+        predicateAB = predicateA.or(predicateB);
+        System.out.println("9는 2또는 3의 배수인가? " + predicateAB.test(9));
+
+        predicateAB = predicateA.negate();
+        System.out.println("9는 홀수인가? " + predicateAB.test(9));
+    }
+}
