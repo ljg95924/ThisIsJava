bd555637104f91f9504611c501ab5becd1b65390
Lee JaeGeun <ljg95924@github.com>
2022-02-03 15:36:02 +0900

    람다식: Function의 순차적 연결

--- src/ThisIsJavaChapter14/FunctionAndThenCompose/FunctionAndThenComposeExample.java
+++ src/ThisIsJavaChapter14/FunctionAndThenCompose/FunctionAndThenComposeExample.java
@@ -1,2 +1,30 @@
-package ThisIsJavaChapter14.FunctionAndThenCompose;public class FunctionAndThenComposeExample {
+package ThisIsJavaChapter14.FunctionAndThenCompose;
+
+import ThisIsJavaChapter14.ConsumerAndThen.Address;
+import ThisIsJavaChapter14.ConsumerAndThen.Member;
+
+import java.util.function.Function;
+
+public class FunctionAndThenComposeExample {
+    public static void main(String[] args) {
+        Function<Member, Address> functionA;
+        Function<Address, String> functionB;
+        Function<Member, String> functionAB;
+        String city;
+
+        functionA = (m) -> m.getAddress();
+        functionB = (a) -> a.getCity();
+
+        functionAB = functionA.andThen(functionB);
+        city = functionAB.apply(
+                new Member("홍길동", "hong", new Address("한국", "서울"))
+        );
+        System.out.println("거주 도시: " + city);
+
+        functionAB = functionB.compose(functionA);
+        city = functionAB.apply(
+                new Member("홍길동", "hong", new Address("한국", "서울"))
+        );
+        System.out.println("거주도시: " + city);
+    }
 }
