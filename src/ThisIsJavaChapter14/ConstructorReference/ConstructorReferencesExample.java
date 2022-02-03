9893cd1a7c436a08780882f752a3c6ced6fb3adc
Lee JaeGeun <ljg95924@github.com>
2022-02-03 19:44:43 +0900

    람다식: 생성자 참조

---
+++ src/ThisIsJavaChapter14/ConstructorReference/ConstructorReferencesExample.java
@@ -0,0 +1,14 @@
+package ThisIsJavaChapter14.ConstructorReference;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ConstructorReferencesExample {
+    public static void main(String[] args) {
+        Function<String, Member> function1 = Member::new;
+        Member member1 = function1.apply("angel");
+
+        BiFunction<String, String, Member> function2 = Member::new;
+        Member member2 = function2.apply("천사", "angel");
+    }
+}
