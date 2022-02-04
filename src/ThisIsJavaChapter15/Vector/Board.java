9be08344af4d82f785f34b9cde5bf67d1bebdfc8
Lee JaeGeun <ljg95924@github.com>
2022-02-04 12:26:35 +0900

    List: Vector

---
+++ src/ThisIsJavaChapter15/Vector/Board.java
@@ -0,0 +1,25 @@
+package ThisIsJavaChapter15.Vector;
+
+public class Board {
+    private String subject;
+    private String content;
+    private String writer;
+
+    public Board(String subject, String content, String writer) {
+        this.subject = subject;
+        this.content = content;
+        this.writer = writer;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+}
