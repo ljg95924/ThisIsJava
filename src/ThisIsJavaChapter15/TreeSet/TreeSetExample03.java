15cd252cb55ffbf3b02e1682af84c8c8ed47d1e6
Lee JaeGeun <ljg95924@github.com>
2022-02-07 11:32:43 +0900

    Tree: 영어단어 정렬하고 범위 검색하기

---
+++ src/ThisIsJavaChapter15/TreeSet/TreeSetExample03.java
@@ -0,0 +1,37 @@
+package ThisIsJavaChapter15.TreeSet;
+
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetExample03 {
+    public static void main(String[] args) {
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("apple");
+        treeSet.add("forever");
+        treeSet.add("description");
+        treeSet.add("ever");
+        treeSet.add("zoo");
+        treeSet.add("base");
+        treeSet.add("guess");
+        treeSet.add("cherry");
+
+        System.out.println("d보다 낮은 단어 검색");
+        NavigableSet<String> lower = treeSet.headSet("d", true);
+        for (String word : lower) {
+            System.out.println(word);
+        }
+
+        System.out.println("d 보다 높은 단어 검색");
+        NavigableSet<String> higher = treeSet.tailSet("d", true);
+        for (String word : higher) {
+            System.out.println(word);
+        }
+
+        System.out.println("c~f 사이의 단어 갬색");
+        NavigableSet<String> rangeSet = treeSet.subSet("c", true, "f", true);
+        for (String word : rangeSet) {
+            System.out.println(word);
+        }
+    }
+}
