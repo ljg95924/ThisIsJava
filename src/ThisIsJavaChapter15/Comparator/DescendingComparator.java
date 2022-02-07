af58a27ed8fb77a79fecea9ac826bcbaadb97aa8
Lee JaeGeun <ljg95924@github.com>
2022-02-07 14:13:04 +0900

    Comparator: Comparable을 구현하지 않고 정렬

---
+++ src/ThisIsJavaChapter15/Comparator/DescendingComparator.java
@@ -0,0 +1,12 @@
+package ThisIsJavaChapter15.Comparator;
+import java.util.Comparator;
+
+public class DescendingComparator implements Comparator<Fruit> {
+
+    @Override
+    public int compare(Fruit o1, Fruit o2) {
+        if (o1.getPrice() < o2.getPrice()) return 1;
+        else if (o1.getPrice() == o2.getPrice()) return 0;
+        else return -1;
+    }
+}
---
+++ src/ThisIsJavaChapter15/Comparator/Fruit.java
@@ -0,0 +1,19 @@
+package ThisIsJavaChapter15.Comparator;
+
+public class Fruit {
+    private String name;
+    private int price;
+
+    public Fruit(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
