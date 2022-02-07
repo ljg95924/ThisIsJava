3df72dd5e9d05fa081bc83be52b6637e31889c41
Lee JaeGeun <ljg95924@github.com>
2022-02-07 14:28:36 +0900

    Stack: pop()

---
+++ src/ThisIsJavaChapter15/Stack/StackExample.java
@@ -0,0 +1,20 @@
+package ThisIsJavaChapter15.Stack;
+
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        Stack<Coin> coinStack = new Stack<Coin>();
+
+        coinStack.push(new Coin(100));
+        coinStack.push(new Coin(50));
+        coinStack.push(new Coin(500));
+        coinStack.push(new Coin(10));
+
+        while (!coinStack.isEmpty()) {
+            Coin coin = coinStack.pop();
+            System.out.println(coin.getValue());
+        }
+    }
+}
+
