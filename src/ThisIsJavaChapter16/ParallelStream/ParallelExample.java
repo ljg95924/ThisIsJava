3aa2b31a569949dd90a60a2613bcdfc94cde58be
Lee JaeGeun <ljg95924@github.com>
2022-02-08 13:34:55 +0900

    Stream: 메소드참조와 병렬처리

--- src/ThisIsJavaChapter16/ParallelStream/ParallelExample.java
+++ src/ThisIsJavaChapter16/ParallelStream/ParallelExample.java
@@ -1,2 +1,26 @@
-package ThisIsJavaChapter16.ParallelStream;public class ParallelExample {
+package ThisIsJavaChapter16.ParallelStream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ParallelExample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("아이유", "고윤하", "성시경", "에픽하이", "하하");
+
+        //순차처리
+        Stream<String> stream = list.stream();
+        stream.forEach(ParallelExample::print); //s -> ParallelExample.print(s) 와 같은표현
+
+        System.out.println();
+
+        //병렬처리
+        Stream<String> parallelStream = list.parallelStream();
+        parallelStream.forEach(ParallelExample::print);
+
+    }
+
+    public static void print(String str) {
+        System.out.println(str + ": " + Thread.currentThread().getName());
+    }
 }
