13f92d3a2d8ed419757a2a96ba5c14fdb498c487
Lee JaeGeun <ljg95924@github.com>
2022-02-08 15:14:36 +0900

    Stream: 스트림파이프라인 중간처리 최종처리

---
+++ src/ThisIsJavaChapter16/StreamPipelines/Member.java
@@ -0,0 +1,24 @@
+package ThisIsJavaChapter16.StreamPipelines;
+
+public class Member {
+    public static int Male = 0;
+    public static int Female = 1;
+    private String name;
+    private int sex;
+    private int age;
+
+    public Member(String name, int sex, int age) {
+
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
