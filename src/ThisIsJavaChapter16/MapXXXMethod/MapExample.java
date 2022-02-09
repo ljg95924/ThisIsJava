41811dfc8fd0f3b04ff9452253a46610f62146f7
Lee JaeGeun <ljg95924@github.com>
2022-02-09 11:08:04 +0900

    stream: mapXXX Method

---
+++ src/ThisIsJavaChapter16/MapXXXMethod/MapExample.java
@@ -0,0 +1,20 @@
+package ThisIsJavaChapter16.MapXXXMethod;
+
+import ThisIsJavaChapter16.LambdaExpressionsForeach.Student;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapExample {
+    public static void main(String[] args) {
+        List<Student> studentList = Arrays.asList(
+                new Student("나윤권", 10),
+                new Student("김진표", 20),
+                new Student("강민경", 30)
+        );
+
+        studentList.stream()
+                .mapToInt(Student::getScore)
+                .forEach(score -> System.out.println(score));
+    }
+}
