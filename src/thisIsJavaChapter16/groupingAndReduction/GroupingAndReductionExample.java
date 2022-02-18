fab31bb1b565842fcb24ba40c352bcfec675c8d6
Lee JaeGeun <ljg95924@github.com>
2022-02-18 15:26:14 +0900

    stream: Groupin 후 Reduction

---
+++ src/thisIsJavaChapter16/groupingAndReduction/GroupingAndReductionExample.java
@@ -0,0 +1,41 @@
+package thisIsJavaChapter16.groupingAndReduction;
+
+import thisIsJavaChapter16.collectMethod.Student;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class GroupingAndReductionExample {
+    public static void main(String[] args) {
+        List<Student> totalList = Arrays.asList(
+                new Student("홍길동", 10, Student.Sex.MALE),
+                new Student("김수애", 6, Student.Sex.FEMALE),
+                new Student("갑을병", 5, Student.Sex.MALE),
+                new Student("박수미", 10, Student.Sex.FEMALE)
+        );
+
+        //성별로 평균 점수를 저장하는 Map 얻기
+        Map<Student.Sex, Double> mapBySex = totalList.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Student::getSex,
+                                Collectors.averagingDouble(Student::getScore)
+                        )
+                );
+
+        System.out.println("여학생 평균 점수: " + mapBySex.get(Student.Sex.FEMALE));
+
+        //성별을 쉼표로 구분한 이름을 저장하는 Map 얻기
+        Map<Student.Sex, String> mapByName = totalList.stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Student::getSex,
+                                Collectors.mapping(Student::getName,
+                                        Collectors.joining(","))
+                        )
+                );
+        System.out.println("여학생 전체 이름: " + mapByName.get(Student.Sex.FEMALE));
+    }
+}
